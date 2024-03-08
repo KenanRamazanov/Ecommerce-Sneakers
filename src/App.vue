5afe22b591d6621a75d96c6001f8d7beca42af2d
kenan551 <kenan.ramazanov1104@gmail.com>
2024-03-08 23:42:28 +0400

    Fixing tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,3 @@
 <template>
-<h1>Hello</h1>
+<h1>Kenan's blog</h1>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
-
