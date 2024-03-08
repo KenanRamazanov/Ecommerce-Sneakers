a512535ff602b810ab435c6c43ca5e10cb365e41
kenan551 <kenan.ramazanov1104@gmail.com>
2024-03-08 23:58:13 +0400

    Editing app

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-<div class=" bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-20">
+<div class=" bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-10">
     <h1>Ramazanov Blog</h1>
 </div>
 </template>
