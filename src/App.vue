82d2f282d0e787a034e8a77627614ca46b72a23c
kenan551 <kenan.ramazanov1104@gmail.com>
2024-03-08 23:59:47 +0400

    Creating header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
 <div class=" bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+
+<header>
+    <div>
+        
+    </div>
+</header>
+
     <h1>Ramazanov Blog</h1>
 </div>
 </template>
