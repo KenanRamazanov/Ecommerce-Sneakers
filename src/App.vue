1b28d296f522977072ea7221a90bbe4b66cc5130
kenan551 <kenan.ramazanov1104@gmail.com>
2024-03-09 12:03:53 +0400

    Final of Header

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
-    <header>
-      <div>
-        <img src="/logo.png" alt="Logo" />
-        <div>
-          <h2>Vue Sneakers</h2>
-          <p>Ən yaxşı idman ayaqqabısı mağazası</p>
+    <header class="flex justify-between border-b border-slate-200 px-10 py-8">
+        <div class="flex items-center gap-4">
+        <img src="/logo.png" alt="Logo" class="w-11" />
+         <div>
+            <h2 class="font-bold text-xl uppercase">Vue Sneakers</h2>
+          <p class="text-slate-400">Shop for the best sneakers</p>
         </div>
       </div>
 
       <ul class="flex items-center gap-10">
         <li class="flex items-center gap-3 cursor-pointer">
           <img src="/cart.svg" alt="Cart" />
-          <b>1205 man.</b>
+          <b>1205 usd</b>
         </li>
         <li class="flex items-center gap-3 cursor-pointer">
           <img src="/heart.svg" alt="Favorite" />
-          <span>Закладки</span>
+          <span>Favorites</span>
         </li>
         <li class="flex items-center gap-3 cursor-pointer">
           <img src="/profile.svg" alt="Favorite" />
-          <span>Профиль</span>
+          <span>Profile</span>
         </li>
       </ul>
     </header>
