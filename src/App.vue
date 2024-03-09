fa05a896dcb2115fc101d22d9591025104c480b3
kenan551 <kenan.ramazanov1104@gmail.com>
2024-03-09 11:47:58 +0400

    Creating Header icons

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <template>
-<div class=" bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
-
-<header>
-    <div>
-        <img src="/logo.png" alt="Logo">
+  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+    <header>
+      <div>
+        <img src="/logo.png" alt="Logo" />
         <div>
-            <h2>Vue Sneakers</h2>
-            <p>Ən yaxşı idman ayaqqabısı mağazası</p>
+          <h2>Vue Sneakers</h2>
+          <p>Ən yaxşı idman ayaqqabısı mağazası</p>
         </div>
-    </div>
-</header>
+      </div>
+
+      <ul class="flex items-center gap-10">
+        <li class="flex items-center gap-3 cursor-pointer">
+          <img src="/cart.svg" alt="Cart" />
+          <b>1205 man.</b>
+        </li>
+        <li class="flex items-center gap-3 cursor-pointer">
+          <img src="/heart.svg" alt="Favorite" />
+          <span>Закладки</span>
+        </li>
+        <li class="flex items-center gap-3 cursor-pointer">
+          <img src="/profile.svg" alt="Favorite" />
+          <span>Профиль</span>
+        </li>
+      </ul>
+    </header>
 
     <h1>Ramazanov Blog</h1>
-</div>
+  </div>
 </template>
