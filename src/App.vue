5aac26b7dcb913abb5a02562a148409ecff1fcb3
kenan551 <kenan.ramazanov1104@gmail.com>
2024-03-09 12:36:37 +0400

    Creating Card

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <script setup>
-import Header from "./components/Header.vue";
+import Header from './components/Header.vue'
 </script>
 
 <template>
   <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
-   <Header/>
+    <Header />
 
-    <h1>Ramazanov Blog</h1>
+    <div>
+      <div>
+       <img src="/like-2.svg" alt="Like 2">
+       <img src="/sneakers/sneakers-1.jpg" alt="Sneakers">
+       <p>Men's Nike Blazer Mid Suede </p>
+
+
+       <div class="flex justify-between">
+         <div>
+            <span>Price:</span>
+            <span>142.99</span>
+         </div>
+
+         <img src="/plus.svg" alt="Plus" />
+       </div>
+       </div>
+    </div>
   </div>
 </template>
-
-
