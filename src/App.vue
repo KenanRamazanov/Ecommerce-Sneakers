e9a11a0bb20a8194e5b99ed61d014480fb3bdab6
kenan551 <kenan.ramazanov1104@gmail.com>
2024-03-10 15:48:02 +0400

    Adding Card to App

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Header from './components/Header.vue'
+import Card from './components/Card.vue'
 </script>
 
 <template>
@@ -7,20 +8,7 @@ import Header from './components/Header.vue'
     <Header />
 
     <div>
-      <div class="relative m-10 border border-slate-100 rounded-3xl p-8 cursor-pointer  hover:-translate-y-2 hover:shadow-xl transition ">
-        <img src="/like-2.svg" alt="Like 2" class="absolute top-8 left-8" />
-        <img src="/sneakers/sneakers-1.jpg" alt="Sneakers" />
-        <p class="mt-2">Men's Nike Blazer Mid Suede</p>
-
-        <div class="flex justify-between mt-5">
-          <div class="flex flex-col gap-2">
-            <span class="text-slate-400">Price:</span>
-            <span class="font-bold">142.99 usd</span>
-          </div>
-
-          <img src="/plus.svg" alt="Plus" />
-        </div>
-      </div>
+    <Card/>
     </div>
   </div>
 </template>
