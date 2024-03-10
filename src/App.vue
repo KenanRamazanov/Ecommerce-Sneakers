5080534381ddfc2fb5a0a956bb3eb446d6ce160e
kenan551 <kenan.ramazanov1104@gmail.com>
2024-03-10 16:01:02 +0400

    Increasing Card

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,18 @@ import Card from './components/Card.vue'
 </script>
 
 <template>
-  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+  <div class="bg-white w-4/5 m-auto  rounded-xl shadow-xl mt-14">
     <Header />
 
     <div>
     <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+
     </div>
   </div>
 </template>
