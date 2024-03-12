c4b8b97940df57046623775949c032062cf84df3
kenan551 <kenan.ramazanov1104@gmail.com>
2024-03-12 22:27:48 +0400

    Adding Drawer to App

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
 import Header from './components/Header.vue'
 import CardList from './components/CardList.vue'
+import Drawer from './components/Drawer.vue'
+
 
 </script>
 
 <template>
+ <Drawer/>
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
     <Header />
 
---
+++ src/components/Drawer.vue
@@ -0,0 +1,3 @@
+<template>
+    <div class="fixed z-10 top-0 h-full w-full bg-black opacity-70" />
+</template>
