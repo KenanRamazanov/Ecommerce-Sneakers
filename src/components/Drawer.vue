250e67a02f9a4bed1ed5650d3dc7eb09f0411ef1
kenan551 <kenan.ramazanov1104@gmail.com>
2024-03-12 22:36:10 +0400

    Final of Drawer

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,3 +1,69 @@
+<script setup>
+import CardList from './CardList.vue';
+</script>
+
 <template>
-    <div class="fixed z-10 top-0 h-full w-full bg-black opacity-70" />
+  <div class="fixed z-10 top-0 h-full w-full bg-black opacity-70" />
+  <div
+    class="flex flex-col justify-between fixed h-full z-10 top-0 h-full right-0 w-96 bg-white px-10 py-7"
+  >
+    <h2 class="text-2xl font-bold mb-10 flex items-center gap-5">
+      <svg
+        class="rotate-180 hover:-translate-x-1 opacity-30 hover:opacity-100 transition cursor-pointer"
+        width="16"
+        height="14"
+        viewBox="0 0 16 14"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M1 7H14.7143"
+          stroke="black"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M8.71436 1L14.7144 7L8.71436 13"
+          stroke="black"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+      Basket
+    </h2>
+    <div class="flex flex-col flex-1 justify-between">
+      <div class="flex flex-col gap-5">
+        <CardList
+          title="Мужские Кроссовки Nike Blazer Mid Suede"
+          price="1000"
+          img="/sneakers/sneakers-1.jpg"
+        />
+      </div>
+
+      <div>
+        <div class="flex flex-col gap-5">
+          <div class="flex items-end gap-2">
+            <span>Total:</span>
+            <div class="flex-1 border-b border-dashed" />
+            <span class="font-bold">1000 $.</span>
+          </div>
+
+          <div class="flex items-end gap-2">
+            <span>Tax 5%:</span>
+            <div class="flex-1 border-b border-dashed" />
+            <span class="font-bold">50 $.</span>
+          </div>
+        </div>
+
+        <button
+          class="flex justify-center items-center gap-3 w-full py-3 mt-10 bg-lime-500 text-white rounded-xl transition active:bg-lime-700 hover:bg-lime-600"
+        >
+        Place an order
+          <img src="/arrow-next.svg" alt="Arrow" />
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
