aa19deb67f3db7bc4b09ad34229de649a13d2d01
kenan551 <kenan.ramazanov1104@gmail.com>
2024-03-12 22:43:54 +0400

    Editing Card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -6,18 +6,20 @@ defineProps({
   isFavorite: Boolean,
   isAdded: Boolean,
   onClickAdd: Function,
-  onClickFavorite: Function,
-
+  onClickFavorite: Function
 })
-
-
 </script>
 
 <template>
   <div
     class="relative border bg-white border-slate-100 rounded-3xl p-8 cursor-pointer hover:-translate-y-2 hover:shadow-xl transition"
   >
-    <img :src="!isFavorite ? '/like-1.svg' : '/like-2.svg'" alt="Favorite" class="absolute top-8 left-8" @click="onClickFavorite" />
+    <img
+      :src="!isFavorite ? '/like-1.svg' : '/like-2.svg'"
+      alt="Favorite"
+      class="absolute top-8 left-8"
+      @click="onClickFavorite"
+    />
 
     <img :src="imageUrl" alt="Sneakers" />
     <p class="mt-2">{{ title }}</p>
@@ -28,7 +30,7 @@ defineProps({
         <span class="font-bold">{{ price }}</span>
       </div>
 
-      <img @click="onClickAdd" :src="!isAdded ? '/plus.svg' : '/checked.svg' " alt="Plus" />
+      <img @click="onClickAdd" :src="!isAdded ? '/plus.svg' : '/checked.svg'" alt="Plus" />
     </div>
   </div>
 </template>
--- src/components/CartItem.vue
+++ src/components/CartItem.vue
@@ -13,7 +13,7 @@ defineProps({
     <div class="flex flex-col w-full">
       <p>{{ title }}</p>
       <div class="flex justify-between mt-5">
-        <span class="font-bold">{{ price }} руб.</span>
+        <span class="font-bold">{{ price }} $.</span>
         <img
           class="cursor-pointer opacity-30 hover:opacity-100 transition"
           src="/close.svg"
