6df6714e778063c068661d55b2b9102a37c64804
kenan551 <kenan.ramazanov1104@gmail.com>
2024-03-13 22:19:57 +0400

    Creating search bar

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,19 @@ import Drawer from './components/Drawer.vue'
 </script>
 
 <template>
- <Drawer/>
+ <!-- <Drawer/> -->
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
     <Header />
 
     <div class="p-10">
+     <div class="flex justify-between">
       <h2 class="text-3xl font-bold mb-8">All Sneakers</h2>
+        <div>
+           <input 
+           placeholder="Search"
+           type="text">
+        </div>
+     </div>
 
      <CardList/>
     </div>
