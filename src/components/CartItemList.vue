042a87d630573337efa308bc72f6804c1ac244fc
kenan551 <kenan.ramazanov1104@gmail.com>
2024-03-13 22:39:07 +0400

    Creating CardItemList

---
+++ src/components/CartItemList.vue
@@ -0,0 +1,22 @@
+<script setup>
+import CardItem from "./CardItem.vue";
+
+
+const onClickAdd = () => {
+    alert("Elave olundu")
+}
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <CardItem 
+    title="Men's Nike Blazer Mid Suede"
+     imageUrl="/sneakers/sneakers-1.jpg" 
+     :price="142.99"
+    :isAdded="true"
+     :isFavorite="true"
+     :onClickAdd="onClickAdd"
+     />
+ 
+  </div>
+</template>
