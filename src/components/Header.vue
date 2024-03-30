5a64ed8657d863296f5672ab433c76b2f3065d7f
kenan551 <kenan.ramazanov1104@gmail.com>
2024-03-30 21:50:34 +0400

    Adding emit to Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,12 @@
+<script setup>
+defineProps({
+  totalPrice: Number
+})
+
+const emit = defineEmits(['openDrawer'])
+</script>
+
+
 <template>
   <header class="flex justify-between border-b border-slate-200 px-10 py-8">
     <div class="flex items-center gap-4">
@@ -9,7 +18,9 @@
     </div>
 
     <ul class="flex items-center gap-10">
-      <li class="flex items-center gap-3 cursor-pointer text-gray-500 hover:text-black">
+      <li
+      @click="() => emit('openDrawer')"
+      class="flex items-center gap-3 cursor-pointer text-gray-500 hover:text-black">
         <img src="/cart.svg" alt="Cart" />
         <b>1205 usd</b>
       </li>
