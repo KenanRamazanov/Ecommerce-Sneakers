82e9d0f419ad47968f262801d39fad4c9b2126ef
kenan551 <kenan.ramazanov1104@gmail.com>
2024-03-30 21:55:33 +0400

    Editing DrawerHeader

--- src/components/DrawerHead.vue
+++ src/components/DrawerHead.vue
@@ -1,8 +1,13 @@
-<script setup></script>
+<script setup>
+import { inject } from 'vue'
+
+const { closeDrawer } = inject('cartActions')
+</script>
 
 <template>
   <div class="flex items-center gap-5 mb-8">
     <svg
+    @click="closeDrawer"
       class="opacity-30 cursor-pointer rotate-180 hover:opacity-100 transition hover:-translate-x-1"
       width="16"
       height="14"
