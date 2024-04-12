814f9eee789b68d851a71b4a9019e191bd63b2f4
kenan551 <kenan.ramazanov1104@gmail.com>
2024-04-12 18:54:44 +0400

    Adding InfoBlock to Drawer

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,15 +1,13 @@
 <script setup>
 import DrawerHead from './DrawerHead.vue'
 import CartItemList from './CartItemList.vue'
-
+import InfoBlock from './InfoBlock.vue'
 const emit = defineEmits('createOrder')
 defineProps({
   totalPrice: Number,
   vatPrice: Number,
   buttonDisabled: Boolean
 })
-
-
 </script>
 
 <template>
@@ -17,8 +15,13 @@ defineProps({
   <div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8">
     <DrawerHead />
 
+    <InfoBlock
+      title="Корзина пустая"
+      description="Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ."
+      image-url="/package-icon.png"
+    />
+
     <CartItemList />
-  
 
     <div class="flex flex-col gap-4 mt-7">
       <div class="flex gap-2">
@@ -34,14 +37,12 @@ defineProps({
       </div>
 
       <button
-      :disabled="buttonDisabled"
-      @click="() => emit('createOrder')"
-        class="mt-4 transition bg-lime-500 w-full rounded-xl py-3  text-white disabled:bg-slate-300 hover:bg-lime-600 active:bg-lime-700 cursor-pointer"
+        :disabled="buttonDisabled"
+        @click="() => emit('createOrder')"
+        class="mt-4 transition bg-lime-500 w-full rounded-xl py-3 text-white disabled:bg-slate-300 hover:bg-lime-600 active:bg-lime-700 cursor-pointer"
       >
-      Place an order
+        Place an order
       </button>
     </div>
-
-
   </div>
 </template>
