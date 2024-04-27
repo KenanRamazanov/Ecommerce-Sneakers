61c7526bc3e3907fa3cf88c4133da3c01614adc0
kenan551 <kenan.ramazanov1104@gmail.com>
2024-04-27 10:01:22 +0400

    Edıtıng Basket

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -11,16 +11,18 @@ defineProps({
 </script>
 
 <template>
-  <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-70"></div>
+  <div  class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-70"></div>
   <div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8">
     <DrawerHead />
 
+    <div v-if="!totalPrice" class="flex h-full items-center">
+
     <InfoBlock
-    v-if="!totalPrice"
       title="Корзина пустая"
       description="Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ."
       image-url="/package-icon.png"
     />
+    </div>
 
     <CartItemList v-if="totalPrice" />
 
